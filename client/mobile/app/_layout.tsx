cdd8770242f3b4a3dd31813cde69d0ce2fd55e6f
Dev Mustafa <dev.mustafa.basheir@gmail.com>
2026-03-31 18:25:18 +0800

    added website and mobile settings work pused

---
+++ client/mobile/app/_layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { ClerkProvider } from '@clerk/expo'
+import { tokenCache } from '@clerk/expo/token-cache'
+import { Slot } from 'expo-router'
+
+export default function RootLayout() {
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+  if (!publishableKey) {
+    throw new Error('Add your Clerk Publishable Key to the .env file')
+  }
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+        <Slot />
+    </ClerkProvider>
+  )
+}
